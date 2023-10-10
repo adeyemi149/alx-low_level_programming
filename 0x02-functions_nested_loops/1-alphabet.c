3327f3f9f4581b29f0bb1c43857887aa4fd4b6ba
adeyemi149 <adeyemi149@gmail.com>
2023-10-10 08:13:13 -0700

    Sign

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
-/*
- * 
- */
+/**
+ *  * print_alphabet - print all alphabet in lowercase
+ *   */
+
 
 void print_alphabet(void)
 {
 	char alphabet;
-	for (alphabet = 'a'; alphabet <= 'z'; alphabet++) {
-		_putchar(alphabet);
-		_putchar('\n');
-	}
 
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		_putchar(alphabet);
+
+	_putchar('\n');
 }
