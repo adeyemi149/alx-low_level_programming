82f3a0119b81791e52fd782a32be642c444db587
adeyemi149 <adeyemi149@gmail.com>
2023-10-10 08:32:39 -0700

    Sign v2

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,7 +6,7 @@
  *    * Return: 1 and prints + if n is greater than zero
  *     * 0 and prints 0 if n is zero
  *      * -1 and prints - if n is less than zero
- *       */
+*       */
 
 int print_sign(int n) 
 {
@@ -18,6 +18,6 @@ int print_sign(int n)
 		return (0);
 	} else if(n < 0)  {
 		_putchar('-');
-		return (-1);
 	}
+	return (-1);
 }
