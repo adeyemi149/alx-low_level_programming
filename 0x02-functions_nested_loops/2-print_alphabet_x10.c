3b487130204a5ac5cdda53ecb3df14f5f6188673
adeyemi149 <adeyemi149@gmail.com>
2023-10-10 08:06:23 -0700

    Sign

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/*
+ *print_alphabet_x10(void) prints alphabet in loxercase 10 times
+ *followed by a new line
+ */
+
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int i = 0;
+	while (i < 10)
+       	{
+		alpha = 'a';
+		while (alpha <= 'z')
+	       	{
+			_putchar(alpha);
+			alpha++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
