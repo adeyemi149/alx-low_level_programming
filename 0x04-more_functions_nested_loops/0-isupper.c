178c70fd0ca345dea7de6f1825de9624179f8a2d
adeyemi149 <adeyemi149@gmail.com>
2023-10-12 03:29:02 -0700

    isUpper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ *_isupper - uppercase letters
+ *@c: check char c
+ *
+ * Return: 0 or 1
+ */
+
+int _isupper(int c)
+{
+	if (c == "A" && c == "Z")
+		return 1;
+	return 0;
+}
