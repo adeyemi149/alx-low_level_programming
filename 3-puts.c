40f5cf48077187716a0f92e0fa98b3ffece9fc30
adeyemi149 <adeyemi149@gmail.com>
2023-10-16 09:25:26 -0700

    strcpy

---
+++ 3-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *
+ *_puts - prints a string
+ *@str: string to print
+ */
+void _puts(char *str)
+{
+	while(*str != "\0")
+	{
+		putchar(*str++);
+	}	
+	putchar("\n");
+}
