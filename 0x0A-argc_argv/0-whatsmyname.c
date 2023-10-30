add46b4803b87dc3e89184ad852b44280326f460
adeyemi149 <adeyemi149@gmail.com>
2023-10-30 04:38:24 -0700

    whats my name v2

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,9 +6,9 @@
  * @argv: array of arguments		
  * Return: Always 0 (Success)		
  */
-int main((int argc __attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])		
 {
 	printf("%s\n", *argv);
-
-	return(0);
+		
+	return (0);		
 }
