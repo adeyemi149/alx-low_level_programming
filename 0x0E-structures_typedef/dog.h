b497fcacb0a87f82e1b6201f4dfabf18f4e7fcb7
adeyemi149 <adeyemi149@gmail.com>
2023-11-06 07:54:05 -0800

    Poppy V4

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,7 +10,7 @@
 	 *
 	 * Description: Longer description
 	 */
-	struct Dog
+	struct dog
 	{
 		char *name;
 		float age;
@@ -21,11 +21,11 @@
 	/**
 	 * dog_t - typedef for struct dog
 	 */
-	typedef struct Dog dog_t;
+	typedef struct dog dog_t;
 
 
-	void init_dog(struct Dog *d, char *name, float age, char *owner);
-	void print_dog(struct Dog *d);
+	void init_dog(struct dog *d, char *name, float age, char *owner);
+	void print_dog(struct dog *d);
 	dog_t *new_dog(char *name, float age, char *owner);
 	void free_dog(dog_t *d);
 	char *_strcpy(char *dest, char *src);
