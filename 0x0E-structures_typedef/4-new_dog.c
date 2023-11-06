1f826c8707b170089ee6f1065497f4ed46fd0fee
adeyemi149 <adeyemi149@gmail.com>
2023-11-06 11:45:01 -0800

    print-dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,12 +13,10 @@ int _strlen(char *s)
 	int i;
 	i = 0;
 
-
 	while (s[i] != '\0')
 	{
 		i++;
 	}
-
 
 	return (i);
 }
@@ -34,9 +32,8 @@ int _strlen(char *s)
  */
 char *_strcpy(char *dest, char *src)
 {
-		int len, i;
-
-		len = 0;
+	int len, i;
+	len = 0;
 
 	while (src[len] != '\0')
 	{
@@ -59,14 +56,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *dog;
 	int len1, len2;
 
-
 	len1 = _strlen(name);
 	len2 = _strlen(owner);
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-
 
 	dog->name = malloc(sizeof(char) * (len1 + 1));
 	if (dog->name == NULL)
@@ -84,7 +79,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	_strcpy(dog->name, name);
 	_strcpy(dog->owner, owner);
 	dog->age = age;
-
 
 	return (dog);	
 }
