aad61eb13a8147aa2293954cfbae77024ddc89bb
adeyemi149 <adeyemi149@gmail.com>
2023-11-24 14:23:17 -0800

    crackme

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -2,21 +2,22 @@
 
 /**
  * flip_bits - Counts the number of bits to change
- * to get from one number to another.
- * @n: First number.
- * @m: Second number.
+ *             to get from one number to another
+ * @n: first number
+ * @m: second number
  *
- * Return: Number of bits to change.
+ * Return: Number of bits to change
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	int a;
-	unsigned int countbit = 0;
+	int a, countbit = 0;
+	unsigned long int current;
 	unsigned long int exclusive = n ^ m;
 
-	for (a = 0; a < (sizeof(unsigned long int) * 8); a++)
+	for (a = 63; a >= 0; a--)
 	{
-		if ((exclusive >> a) & 1)
+		current = exclusive >> a;
+		if (current & 1)
 			countbit++;
 	}
 
